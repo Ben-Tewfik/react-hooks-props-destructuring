624710c21c0f1b11a765e54f5cefca794f26ec90
Benarba Tewfik <tewfikhotspur@gmail.com>
2023-08-30 21:40:55 +0200

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,17 @@ import React from "react";
 import MovieCard from "./MovieCard";
 
 function App() {
-  const title = "Mad Max";
+  const movieTitle = "Mad Max";
   const posterURL =
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
   const genresArr = ["Action", "Adventure", "Science Fiction", "Thriller"];
-
+  const socialLinks = {
+    github: "https://github.com/liza",
+    linkedin: "https://www.linkedin.com/in/liza/",
+  };
   return (
     <div className="App">
-      <MovieCard title={title} genres={genresArr} />
+      <MovieCard title={movieTitle} genres={genresArr} links={socialLinks} />
     </div>
   );
 }
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,12 +4,18 @@ function MovieCard({
   title,
   posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
   genres,
+  links,
 }) {
+  const { github, linkedin } = links;
   return (
     <div className="movie-card">
       <img src={posterSrc} alt={title} />
       <h2>{title}</h2>
       <small>{genres.join(", ")}</small>
+      <div>
+        <a href={github}>My Github</a>
+        <a href={linkedin}>My linkedin</a>
+      </div>
     </div>
   );
 }
